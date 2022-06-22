a284bafd8a927aef41782f6ecef402f766aad464
kimayeon <tofu201311011@gmail.com>
2022-06-22 13:56:15 +0900

    testController, testService, testDAO, test_mapper 내용 수정 및 추가

--- src/main/java/com/fullstack/cbt/dao/TestDAO.java
+++ src/main/java/com/fullstack/cbt/dao/TestDAO.java
@@ -1,5 +1,8 @@
 package com.fullstack.cbt.dao;
 
+import java.util.ArrayList;
+
 public interface TestDAO {
 
+	ArrayList<String> subjectList();
 }
--- src/main/java/com/fullstack/cbt/service/TestService.java
+++ src/main/java/com/fullstack/cbt/service/TestService.java
@@ -1,8 +1,27 @@
 package com.fullstack.cbt.service;
 
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fullstack.cbt.dao.TestDAO;
 
 @Service
 public class TestService {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired TestDAO dao;
+	
+	public ArrayList<String> subjectList() {
+		logger.info("등록된 과목 가져오기 서비스 요청");
+		ArrayList<String> subject = dao.subjectList();
+		return subject;
+	}
+
+	
+	
 }

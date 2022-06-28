761d3864b7185664a8631b884c38076601836965
kdb <letspro92@gmail.com>
2022-06-28 09:58:25 +0900

    시험 응시(문제출제 알고리즘 포함) 작업

--- src/main/java/com/fullstack/cbt/controller/TestController.java
+++ src/main/java/com/fullstack/cbt/controller/TestController.java
@@ -1,6 +1,12 @@
 package com.fullstack.cbt.controller;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fullstack.cbt.dto.ProblemDTO;
+import com.fullstack.cbt.dto.SubjectChapterDTO;
 import com.fullstack.cbt.dto.SubjectDTO;
 import com.fullstack.cbt.service.TestService;
 
@@ -35,10 +43,193 @@ public class TestController {
 	}	
 	
 	@RequestMapping(value = "/test.do", method = RequestMethod.POST)
-	public String test(Model model, @RequestParam String su_idx) {
+	public String test(Model model, @RequestParam String su_idx, HttpSession session) {
 		logger.info("과목 선택하기");
 		logger.info("선택한 과목 고유번호 : " + su_idx);
-		return "test"; 
-	}	
+		
+		String loginId = (String) session.getAttribute("loginId");
+		
+		ArrayList<SubjectChapterDTO> subjChap = service.subjectChapterList(su_idx);//선택한 과목에 대한 과목 단원수(단, 출제된 문제 데이터가 있을 경우) 가져오기
+		
+		/* 시험문제 출제 알고리즘 - 랜덤 방식, 과목 단원 및 난이도별로 문제 출제, 기존에 출제되었던 문제는 빈도를 낮추어 총 10문제가 출제되어야 한다.
+		 * 랜덤 방식 - SQL 랜덤으로 가져오는 방식 적용(랜덤으로 가져와야 우선순위가 계속 변동된다)
+		 * 과목 단원별 출제 - 사용자가 선택한 과목의 총 과목 단원수를 구해와서 출제할 총 문제수(10개)를 나눔(그래야 골고루 출제가 된다.)
+		 * 난이도별 출제 - 난이도는 상/중/하로 나누어 각각 3/3/4로 균등하게 출제할 수 있다.
+		 * 기존에 출제되었던 문제는 일단 제외 시키는 로직이다. 해당 단원에서 3회 추가 문제 출제 요청을 한다.(추가 문제 출제도 없을 경우 난이도는 변경 없이 다음 단원으로 넘어간다)
+		 */
+		
+		ArrayList<ProblemDTO> problemList = new ArrayList<ProblemDTO>();//출제될 문제 목록
+		session.setAttribute("testStart", service.getCurrentTimestamp());
+		
+		//초기 문제의 난이도는 상/중/하 중 랜덤으로 설정하기 위해서
+		Random rd = new Random();
+        String randomLevel = "상중하";
+		char problemLevel = randomLevel.charAt(rd.nextInt(randomLevel.length()));
+		
+		//문제출제를 위한 무한반복문
+		while(problemList.size() < 10) {
+			for(int i=0; i<(int) Math.ceil(10 / subjChap.size()); i++) {
+				//문제수 초과 방지 코드
+				if(problemList.size() >= 10) {
+					break;
+				}
+				
+				//선택한 과목의 과목단원별로 등록된 문제 데이터 가져오기
+				for(int j=0; j<subjChap.size(); j++) {
+					//문제수 초과 방지 코드
+					if(problemList.size() >= 10) {
+						break;
+					}
+					
+					try {
+						//서비스에 요청하기 위한 해시맵 생성
+						HashMap<String, Object> map = new HashMap<String, Object>();
+						map.put("su_idx", su_idx);
+						map.put("sc_idx", subjChap.get(j).getSc_idx());
+						map.put("pc_difficulty", problemLevel);
+						
+						List<Integer> idx = new ArrayList<Integer>();
+						for (ProblemDTO item : problemList) {
+							if(item != null) {
+								idx.add(item.getPc_idx());
+							}
+						}
+						map.put("problemList", idx);
+						
+						//과목단원/난이도에 맞는 문제 데이터를 랜덤으로 가져오기
+						ProblemDTO problem = service.getProblem(map);
+						
+						//아래 로직은 출제되었던 문제 빈도수를 낮추기 위한 로직이다
+						int k = 0;
+						while(k<3) {
+							k++;
+							
+							//이전에 출제되었던 문제인지 확인하기
+							int isBeforeCnt = service.isBeforeProblem(problem.getPc_idx(), loginId);
+							
+							if(isBeforeCnt < 1) {//출제되었던 문제가 아니라면
+								break;//계속 진행
+							} else {//출제된 문제라면
+								idx.add(problem.getPc_idx());
+								map.put("problemList", idx);//해당 문제를 제외하기
+								
+								problem = service.getProblem(map);//서비스에 문제출제 재요청
+							}
+						}
+						
+						//출제될 문제 목록에 삽입
+						if(problem != null) {
+							problemList.add(problem);
+						}
+						
+						//난이도별 출제를 위해서 다음 문제의 난이도 설정하는 부분
+						switch(problemLevel) {
+						case '상':
+							problemLevel = '중';
+							break;
+							
+						case '중':
+							problemLevel = '하';
+							break;
+							
+						case '하':
+							problemLevel = '상';
+							break;
+							
+						default:
+							break;
+						}
+					} catch (NullPointerException e) {
+						continue;
+					}
+					
+				}
+			}
+			
+			//문제출제 알고리즘에 의한 필터를 거친 로직에 의존하면 출제할 문제가 부족하기 때문에 부족한 문제만큼 추가로 출제하는 로직이 필요하다.
+			for(int i=0; i<10-problemList.size(); i++) {
+				//서비스에 요청하기 위한 해시맵 생성
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("su_idx", su_idx);
+				List<Integer> idx = new ArrayList<Integer>();
+				
+				for (ProblemDTO item : problemList) {
+					if(item != null) {
+						idx.add(item.getPc_idx());
+					}
+				}
+				
+				map.put("problemList", idx);
+				
+				ProblemDTO problem = service.getProblem(map);
+				
+				if(problem != null) {
+					problemList.add(problem);
+				}
+			}
+		}
+		
+		model.addAttribute("su_idx", su_idx);
+		model.addAttribute("problemList", problemList);
+		
+		return "test";
+	}
+	
+	@RequestMapping(value = "/testReg.do", method = RequestMethod.POST)
+	public String testReg(Model model, @RequestParam HashMap<String, Object> params, HttpSession session) {
+		int su_idx = Integer.parseInt(String.valueOf(params.get("su_idx")));//과목 고유번호
+		String mb_id = (String) session.getAttribute("loginId");
+		
+		int tt_times = service.getTestTimes(su_idx, mb_id);
+		int tt_score = 0;//점수
+		
+		//시험 채점(점수 계산)
+		for(int i=1; i<=10; i++) {
+			if(params.get("pc_answer"+i) != null) {
+				int choiceNo = (int) Integer.parseInt(String.valueOf(params.get("pc_answer"+i)));
+				int answer = service.getProblemAnswer((int) Integer.parseInt(String.valueOf(params.get("idx"+i))));
+				
+				if(choiceNo == answer) {
+					tt_score += 10;
+				}
+			}
+		}
+		
+		String tt_status = "제출완료";//제출상태
+		
+		params.put("tt_times", tt_times);
+		params.put("tt_score", tt_score);
+		
+		Timestamp tt_start_date = Timestamp.valueOf((String) session.getAttribute("testStart"));
+		params.put("tt_start_date", tt_start_date);
+		
+		params.put("tt_status", tt_status);
+		params.put("mb_id", mb_id);
+		
+		int tt_idx = service.testReg(params);//시험 고유번호
+		
+		if(tt_idx > 0) {
+			//시험 답안 제출
+			for(int i=1; i<=10; i++) {
+				int choiceNo = params.get("pc_answer"+i) != null ? (int) Integer.parseInt(String.valueOf(params.get("pc_answer"+i))) : 0;
+				int answer = service.getProblemAnswer((int) Integer.parseInt(String.valueOf(params.get("idx"+i))));
+				char isAnswer = choiceNo == answer ? 'O' : 'X';
+				
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				
+				map.put("ta_problem_no", i);
+				map.put("ta_choice_no", choiceNo);
+				map.put("ta_is_answer", isAnswer);
+				map.put("pc_idx", params.get("idx"+i));
+				map.put("tt_idx", tt_idx);
+				
+				boolean result = service.testAnswerReg(map);
+			}
+			
+			model.addAttribute("tt_idx", tt_idx);
+		}
+		
+		return "testResult";
+	}
 	
 }

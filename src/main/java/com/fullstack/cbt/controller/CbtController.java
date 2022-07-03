6b20648c5db9ad3b3cfce384743ecfd54b1d4402
kimayeon <tofu201311011@gmail.com>
2022-07-03 23:56:56 +0900

    cbt 시험관리쪽 관리자 유효성 체크 추가

--- src/main/java/com/fullstack/cbt/controller/CbtController.java
+++ src/main/java/com/fullstack/cbt/controller/CbtController.java
@@ -31,13 +31,12 @@ public class CbtController {
 	@RequestMapping(value = "/cbtList.do", method = RequestMethod.GET)
 	public String cbtList(Model model, HttpSession session,Criteria cri) {
 		
-		//String page ="login";
-		//logger.info("관리자 로그인 ? : " + session.getAttribute("isAdmin"));
-		/*
-		if(!session.getAttribute("isAdmin").equals("true")) {
-			model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
-		}else {}
-		*/
+		String page ="login";
+		logger.info("관리자 로그인 ? : " + session.getAttribute("isAdmin"));
+		String adminId = (String) session.getAttribute("isAdmin");
+		
+		
+		if(adminId != null) {
 			//select 과목명 가져오기
 			ArrayList<SubjectDTO> subjectList =service.subjectList(); 
 			logger.info("등록된 과목 가져오기 : " + subjectList.size());
@@ -55,7 +54,11 @@ public class CbtController {
 			
 			PageMakerDTO pageMake = new PageMakerDTO(cri, total);
 			model.addAttribute("pageMaker", pageMake);
-			String page = "adminTestList";
+			page = "adminTestList";
+		}else {
+			model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
+		}
+		
 		
 			
 			return page; 
@@ -66,47 +69,52 @@ public class CbtController {
 	//선택한 값에 따른 리스트 가져오기 
 		@RequestMapping(value = "/cbtFormList.do")
 		public String cbtForm(Model model,HttpSession session,Criteria cri,@RequestParam String su_idx, String tt_status, String mb_id, int pageNum) {
-			/*ring page ="login";
-			if(!session.getAttribute("isAdmin").equals("true")) {
-				model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
-			}else {}
-			*/
-				//과목명 셀렉트박스 불러오기
-				ArrayList<SubjectDTO> subjectList =service.subjectList(); 
-				logger.info("등록된 과목 가져오기 : " + subjectList.size());
-				
-				if(subjectList.size()>0) {
-					model.addAttribute("subjectList", subjectList);
-				} 
-				
-				
-				logger.info("과목번호 : " + su_idx);
-				logger.info("제출상태 : " + tt_status);
-				logger.info("아이디 : " + mb_id);
-				logger.info("pageNum :" + pageNum);
-				
-				// 사용자가 선택 후 값을 고정해주기 위해 model에 저장 
-				model.addAttribute("idx", su_idx);
-				model.addAttribute("status", tt_status);
-				model.addAttribute("id", mb_id);
-				
-				
-				//선택된 값에 따라 리스트 블러오기 
-				int skip = (pageNum -1) * 10;
-				ArrayList<TestDTO> testdto = service.selectedListPaging(su_idx,tt_status,mb_id,skip);
-				model.addAttribute("testdto", testdto);
-				
-				
-				int selectedTotal = service.selectedTotal(su_idx,tt_status,mb_id);
-				model.addAttribute("listCnt", selectedTotal);
-				logger.info("선택된 게시글 수 : " + selectedTotal);
-				
-				//생성자 (int pageNum, int total)
-				PageMakerDTO pageMake2 = new PageMakerDTO(pageNum, selectedTotal);
-				model.addAttribute("pageMaker", pageMake2);
-				
-				String page = "adminTestList";
 			
+				String page ="login";
+				logger.info("관리자 로그인 ? : " + session.getAttribute("isAdmin"));
+				String adminId = (String) session.getAttribute("isAdmin");
+			
+				if(adminId != null) {
+					
+					//과목명 셀렉트박스 불러오기
+					ArrayList<SubjectDTO> subjectList =service.subjectList(); 
+					logger.info("등록된 과목 가져오기 : " + subjectList.size());
+					
+					if(subjectList.size()>0) {
+						model.addAttribute("subjectList", subjectList);
+					} 
+					
+					
+					logger.info("과목번호 : " + su_idx);
+					logger.info("제출상태 : " + tt_status);
+					logger.info("아이디 : " + mb_id);
+					logger.info("pageNum :" + pageNum);
+					
+					// 사용자가 선택 후 값을 고정해주기 위해 model에 저장 
+					model.addAttribute("idx", su_idx);
+					model.addAttribute("status", tt_status);
+					model.addAttribute("id", mb_id);
+					
+					
+					//선택된 값에 따라 리스트 블러오기 
+					int skip = (pageNum -1) * 10;
+					ArrayList<TestDTO> testdto = service.selectedListPaging(su_idx,tt_status,mb_id,skip);
+					model.addAttribute("testdto", testdto);
+					
+					
+					int selectedTotal = service.selectedTotal(su_idx,tt_status,mb_id);
+					model.addAttribute("listCnt", selectedTotal);
+					logger.info("선택된 게시글 수 : " + selectedTotal);
+					
+					//생성자 (int pageNum, int total)
+					PageMakerDTO pageMake2 = new PageMakerDTO(pageNum, selectedTotal);
+					model.addAttribute("pageMaker", pageMake2);
+					
+					page = "adminTestList";
+					
+				}else{
+					model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
+				}
 			
 			
 			
@@ -121,26 +129,31 @@ public class CbtController {
 		//상세보기 
 		@RequestMapping(value = "/cbtTestDetail.do")
 		public String cbtTestDetail(Model model, HttpSession session,@RequestParam String tt_idx) {
-			/*
-			String page ="login";
-			if(!session.getAttribute("isAdmin").equals("true")) {
-				model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
-			}else {}
-			*/
-				logger.info("시험 고유번호 : " +tt_idx);
-				//시험고유번호가 #인 과목명, 회차, 시험시작일자,제출일자, 점수, 상태 
-				TestDTO testInfo = service.testInfo(tt_idx);
-				if(testInfo != null) {
-					model.addAttribute("testInfo", testInfo);
-				}
 				
-				//시험고유번호가 #인 문제출제고유번호, 문제1~10, 사용자답안1~10, 정답오답여부10개 , 문제출제 고유번호에 따른 정답(10개)과 각 사지선다 문항(10)
-				ArrayList<ProblemDTO> testDetail = service.testDetail(tt_idx); 
-				logger.info("데이터 개수 : " + testDetail.size());
-				if(testDetail.size()>0) {
-					model.addAttribute("testDetail", testDetail);
+				String page ="login";
+				logger.info("관리자 로그인 ? : " + session.getAttribute("isAdmin"));
+				String adminId = (String) session.getAttribute("isAdmin");
+			
+				if(adminId != null) {
+					
+					logger.info("시험 고유번호 : " +tt_idx);
+					//시험고유번호가 #인 과목명, 회차, 시험시작일자,제출일자, 점수, 상태 
+					TestDTO testInfo = service.testInfo(tt_idx);
+					if(testInfo != null) {
+						model.addAttribute("testInfo", testInfo);
+					}
+					
+					//시험고유번호가 #인 문제출제고유번호, 문제1~10, 사용자답안1~10, 정답오답여부10개 , 문제출제 고유번호에 따른 정답(10개)과 각 사지선다 문항(10)
+					ArrayList<ProblemDTO> testDetail = service.testDetail(tt_idx); 
+					logger.info("데이터 개수 : " + testDetail.size());
+					if(testDetail.size()>0) {
+						model.addAttribute("testDetail", testDetail);
+					}
+					page = "adminTestDetail";
+					
+				}else {
+					model.addAttribute("msg","관리자 계정 로그인이 필요한 서비스입니다.");
 				}
-				String page = "adminTestDetail";
 				
 			
 			
